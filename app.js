b0fd0f1ed1d3250348574061fc05e851dc7369cb
Haider <mhaiderasif@outlook.com>
2024-04-23 13:09:32 +0100

    added a contact page ref Haider

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
-//#region Require statements 
-const express = require('express');
-const path = require('path');
-const custom_bs = require('./utils/browserSync')
+//#region Require statements
+const express = require("express");
+const path = require("path");
+const custom_bs = require("./utils/browserSync");
 //#endregion
 
 const app = express();
 custom_bs.initialiseBrowserSync();
 
 //#region Adding static files to app
-app.use("/css", express.static('./public/css'));
-app.use("/js", express.static('./public/js'));
-app.use("/img", express.static('./public/img'));
-app.use("/bootstrap", express.static('./node_modules/bootstrap/dist'));
-app.use("/utils", express.static('./utils'))
+app.use("/css", express.static("./public/css"));
+app.use("/js", express.static("./public/js"));
+app.use("/img", express.static("./public/img"));
+app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
+app.use("/utils", express.static("./utils"));
 //#endregion
 
 //setting the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //#region Configuring the app routes
 
 //home page route
-app.get('/', (req, res) => {
-    res.render('home', req.query);
+app.get("/", (req, res) => {
+  res.render("home", req.query);
 });
 
-//getJobSeekers
-app.get('/getJobSeekers', (req, res) => {
-    let jobseekers = ["Stareo Mark", "Marshal Jorun", "Marko Patel", "Nisha Stuash"]
-    res.render('getJobSeekers', { items: jobseekers }, (error, ejsPage) => {
-            if(error){
-                console.log(`The error thrown by page : ${error.message}`);
-                res.status(500).send('An error occurred');
-            }else{
-                res.send(ejsPage)
-            }
-    });
+//getJobSeekers for recruiters
+app.get("/getJobSeekers", (req, res) => {
+  let jobseekers = [
+    "Stareo Mark",
+    "Marshal Jorun",
+    "Marko Patel",
+    "Nisha Stuash",
+  ];
+  res.render("getJobSeekers", { items: jobseekers }, (error, ejsPage) => {
+    if (error) {
+      console.log(`The error thrown by page : ${error.message}`);
+      res.status(500).send("An error occurred");
+    } else {
+      res.send(ejsPage);
+    }
+  });
 });
 
-//showJobPosts for jobSeekers
-app.get('/getJobPosts', (req, res) => {
-    let jobs = ["job1", "job2", "job3", "job4"]
-    res.render('getJobPosts', { items: jobs }, (error, ejsPage) => {
-            if(error){
-                console.log(`The error thrown by page : ${error.message}`);
-                res.status(500).send('An error occurred');
-            }else{
-                res.send(ejsPage)
-            }
-    });
+//getJobPosts for jobSeekers
+app.get("/getJobPosts", (req, res) => {
+  let jobs = ["job1", "job2", "job3", "job4"];
+  res.render("getJobPosts", { items: jobs }, (error, ejsPage) => {
+    if (error) {
+      console.log(`The error thrown by page : ${error.message}`);
+      res.status(500).send("An error occurred");
+    } else {
+      res.send(ejsPage);
+    }
+  });
 });
+
+//contactUs Page
+app.get("/contactUs", (req, res) => {
+  res.render("contactUs");
+});
+
 //#endregion
 
-app.listen(5050, () =>{
-    console.log('App is listening on port 5050');
+app.listen(5050, () => {
+  console.log("App is listening on port 5050");
 });
